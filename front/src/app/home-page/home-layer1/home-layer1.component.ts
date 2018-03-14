4e1a72513a4ce9d0a7f9d584ab16db042c712883
kaa_alex <cops2002@mail.ru>
2018-03-14 11:25:10 +0300

    Cтруктура проекта

---
+++ front/src/app/home-page/home-layer1/home-layer1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-layer1',
+  templateUrl: './home-layer1.component.html',
+  styleUrls: ['./home-layer1.component.css']
+})
+export class HomeLayer1Component implements OnInit {
+  goFoodsBtn = 'Перейти к продуктам';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
