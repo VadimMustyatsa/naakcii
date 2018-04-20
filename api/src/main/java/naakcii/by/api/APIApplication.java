fae88401404a29bb78af7eb80275fbfe3ae0e48e
kaaAlexander <35295520+kaaAlexander@users.noreply.github.com>
2018-04-20 12:34:36 +0300

    Update APIApplication.java

--- api/src/main/java/naakcii/by/api/APIApplication.java
+++ api/src/main/java/naakcii/by/api/APIApplication.java
@@ -3,6 +3,24 @@ package naakcii.by.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Bean
+
+ public WebMvcConfigurer corsConfigurer() {
+
+  return new WebMvcConfigurerAdapter() {
+
+   @Override
+
+   public void addCorsMappings(CorsRegistry registry) {
+
+    registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "HEADER");
+
+   }
+
+  };
+
+ }
+
 @SpringBootApplication
 public class APIApplication {
 
