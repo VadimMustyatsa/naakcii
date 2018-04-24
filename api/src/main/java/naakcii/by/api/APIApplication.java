6f15e697c5b88eefaddad056e5b74c857b7da06e
kaa_alex <cops2002@mail.ru>
2018-04-24 16:07:41 +0300

    API v0.1.1 Complete, properties for web-server

--- api/src/main/java/naakcii/by/api/APIApplication.java
+++ api/src/main/java/naakcii/by/api/APIApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 @SpringBootApplication
 
 public class APIApplication {
