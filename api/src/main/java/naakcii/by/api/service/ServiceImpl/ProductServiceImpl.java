a3d92fdf7ed616e9afa3cc154c27a74032f9a0c0
kaaAlexander <35295520+kaaAlexander@users.noreply.github.com>
2018-04-26 14:19:17 +0300

    Update ProductServiceImpl.java

--- api/src/main/java/naakcii/by/api/service/ServiceImpl/ProductServiceImpl.java
+++ api/src/main/java/naakcii/by/api/service/ServiceImpl/ProductServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 @Service
 public class ProductServiceImpl implements ProductService {
