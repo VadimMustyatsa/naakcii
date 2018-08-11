1a16a63e243db9241c4c44028f0c86423f86f1fd
Dmitry Isaevski <d.isayevskiy@softteco.com>
2018-08-11 07:59:07 +0300

    correct partner and diplom text, add scroll-to-top button, minor css changes

--- front/src/app/home-page/home-page.component.ts
+++ front/src/app/home-page/home-page.component.ts
@@ -1,17 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  animations: [
+    trigger('getData', [
+      state('inactive', style({
+        opacity: 0
+      })),
+      state('active',   style({
+        opacity: 1
+      })),
+      transition('inactive => active', animate('500ms ease-in')),
+    ])
+  ]
 })
 export class HomePageComponent implements OnInit {
+
+  getData = 'inactive';
 
   constructor(private titleService: Title) { }
 
   ngOnInit() {
     this.titleService.setTitle('Сервис экономии – naakcii.by')
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      document.getElementById("up-btn").style.display = "block";
+      this.getData = 'active';
+    } else {
+      document.getElementById("up-btn").style.display = "none";
+      this.getData = 'inactive';
+    }
+  }
+
+  clickToTop() {
+    document.documentElement.scrollTop = 0;
   }
 
 }
