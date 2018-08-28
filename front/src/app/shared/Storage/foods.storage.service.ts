ec435801029a3ba57753472bd7c49f01e6e3c97e
Dmitry Isaevski <d.isayevskiy@softteco.com>
2018-08-28 20:06:20 +0300

    correct api-switcher, change prompt text for shops

--- front/src/app/shared/Storage/foods.storage.service.ts
+++ front/src/app/shared/Storage/foods.storage.service.ts
@@ -5,13 +5,13 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class FoodsStorageService {
-  private storeUrl = 'http://' + window.location.hostname +':8080/api/chain';
-  // private storeUrl = 'http://178.124.206.42:8080/api/chain';
+  private storeUrl: string;
 
   constructor(private http: HttpClient) {
   }
 
   getAll() {
+    this.storeUrl = window.location.hostname === 'localhost' ? 'http://178.124.206.42:8080/api/chain' : 'http://' + window.location.hostname +':8080/api/chain';
     return this.http.get<Storag[]>(this.storeUrl)
       .map(chainList => {
         return chainList.map(chain => {
