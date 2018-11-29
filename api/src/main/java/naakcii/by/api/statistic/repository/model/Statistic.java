e41a2ff2717af1fdc4c0959ae33277d93c3522ed
cops2002@mail.ru <fuckyouspielberg>
2018-11-29 22:57:40 +0300

    Speedtest

--- api/src/main/java/naakcii/by/api/statistic/repository/model/Statistic.java
+++ api/src/main/java/naakcii/by/api/statistic/repository/model/Statistic.java
@@ -1,13 +1,13 @@
 package naakcii.by.api.statistic.repository.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "v_discount_counter")
 public class Statistic implements Serializable {
+    @Id
+    @GeneratedValue(generator = "ID_GENERATOR")
     @Column(name = "id")
     private Long id;
     @Column(name = "chain")
