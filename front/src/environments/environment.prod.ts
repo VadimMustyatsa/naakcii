9410200ee2150005b53a60b6089ac3de05092338
Dmitry Isaevski <d.isayevskiy@softteco.com>
2018-12-10 13:42:24 +0300

    change prod urls

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://178.124.206.54:8443'
-  // baseUrl: 'http://178.124.206.54:8080'
+  // baseUrl: 'https://178.124.206.54:8443'
+  baseUrl: 'http://178.124.206.54:8080'
 };
