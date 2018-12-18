6654269d7c7eacc76ce151bda3cdab0cddc8df63
kaaAlexander <35295520+kaaAlexander@users.noreply.github.com>
2018-12-18 17:11:38 +0300

    Update StatisticController.java

--- api/src/main/java/naakcii/by/api/statistic/controller/StatisticController.java
+++ api/src/main/java/naakcii/by/api/statistic/controller/StatisticController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-@RequestMapping({"/statistic"})
+@RequestMapping({"/statistics"})
 public class StatisticController {
 
     @Autowired
