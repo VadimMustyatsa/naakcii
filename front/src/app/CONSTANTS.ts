1db4804243df85ffed5015afa836f6b7090afd2c
griboedovvladimir <likecoffee@yandex.ru>
2018-12-21 01:48:47 +0300

    fixed modal

--- front/src/app/CONSTANTS.ts
+++
@@ -1,6 +0,0 @@
-/*----------SESSIONSSTORAGE_CONSTANTS--------------*/
-export const SESSION_STORAGE_KEY_EMAIL = 'SESSION_STORAGE_KEY_EMAIL';
-export const STORAGE_KEY = "naakciiStorage";
-export const STORAGE_KEY_COUNT = "naakciiStorageCount";
-export const CHAIN_STORAGE_KEY = 'naakciiChainStorage';
-export const UNDISCOUNT_STORAGE_KEY = 'naakciiUndiscountStorage';
