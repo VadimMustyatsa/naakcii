ab0cc137a11816230b40f99366ffcc22941773c4
AlexandrKuchura <kuchura0409@gmail.com>
2019-01-19 22:43:48 +0300

    Logger was added Slack snippet notification was added Set parser task for 3.15. Ftp properties was added for Linux/Windows

--- src/main/java/naakcii/by/api/util/tasks/DataParserTask.java
+++ src/main/java/naakcii/by/api/util/tasks/DataParserTask.java
@@ -4,8 +4,10 @@ import naakcii.by.api.chain.ChainRepository;
 import naakcii.by.api.util.parser.IDataParser;
 import naakcii.by.api.util.parser.multisheet.ParsingResult;
 import naakcii.by.api.util.slack.SlackNotification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,54 +21,59 @@ import java.util.regex.Pattern;
 @Component
 public class DataParserTask {
 
-    @Autowired
+    private static final Logger logger = LogManager.getLogger(DataParserTask.class);
     private SlackNotification slackNotification;
-
-    @Autowired
     private ChainRepository chainRepository;
+    private IDataParser dataParser;
+    private Environment env;
 
     @Autowired
-    private IDataParser dataParser;
+    public DataParserTask(SlackNotification slackNotification, ChainRepository chainRepositor,
+                          IDataParser dataParser, Environment env) {
+        this.slackNotification = slackNotification;
+        this.chainRepository = chainRepositor;
+        this.dataParser = dataParser;
+        this.env = env;
+    }
 
-    @Value("${ftp.action.data.folder.name}")
-    private String actionDataFolder;
-
-    @Value("${ftp.action.data.linux.home.path}")
-    private String linuxPath;
-
-    @Value("${ftp.action.data.windows.home.path}")
-    private String windowsPath;
-
-    //@Scheduled(cron = "0 15 3 * * *")
-    @Scheduled(cron = "0/20 * * * * *")
+    @Scheduled(cron = "0 15 3 * * *")
     public void parsingTask() {
+        logger.info("Parsing start:");
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat date = new SimpleDateFormat("yyyy_MM_dd");
         StringBuilder fileHomePath = getHomePath();
         slackNotification.sendMessageToNotificationsChannel("_*Parsing start:*_");
         List<String> synonyms = chainRepository.getAllSynonyms();
         for (String chainSynonym : synonyms) {
+            logger.info("Scan folder: " + chainSynonym);
             slackNotification.sendMessageToNotificationsChannel("*Scan folder:* _" + chainSynonym + "_");
             StringBuilder currentChainFolderPath = new StringBuilder(fileHomePath);
             currentChainFolderPath.append(chainSynonym)
                     .append(File.separator)
-                    .append(actionDataFolder)
+                    .append(env.getProperty("ftp.action.data.folder.name"))
                     .append(File.separator);
             File folder = new File(currentChainFolderPath.toString());
-            File[] folderFiles = folder.listFiles();
-            if (folderFiles != null) {
-                for (File file : folderFiles) {
-                    Pattern pattern = Pattern.compile(".*" + date.format(calendar.getTime()) + "\\.xlsx");
-                    Matcher matcher = pattern.matcher(file.getName());
-                    if (matcher.find()) {
-                        slackNotification.sendMessageToNotificationsChannel("Next file was parsed: `" + file.getName() + "`");
-                        List<ParsingResult<?>> parsingResults = dataParser.parseActionProducts(file.getPath(), chainSynonym);
-                        parsingResults.forEach((ParsingResult<?> result) ->
-                                slackNotification.sendMessageToNotificationsChannel("```" + result.toString() + "```"));
-                    } else {
-                        slackNotification.sendMessageToNotificationsChannel("Next file was skipped: " + file.getName());
+            try {
+                File[] folderFiles = folder.listFiles();
+                if (folderFiles != null) {
+                    for (File file : folderFiles) {
+                        Pattern pattern = Pattern.compile(".*" + date.format(calendar.getTime()) + "\\.xlsx");
+                        Matcher matcher = pattern.matcher(file.getName());
+                        if (matcher.find()) {
+                            logger.info("Next file was parsed: " + file.getName());
+                            slackNotification.sendMessageToNotificationsChannel("Next file was parsed: `" + file.getName() + "`");
+                            List<ParsingResult<?>> parsingResults = dataParser.parseActionProducts(file.getPath(), chainSynonym);
+                            parsingResults.forEach((ParsingResult<?> result) ->
+                                    slackNotification.sendCodeSnippetToNotificationsChannel(result.toString()));
+                        } else {
+                            logger.info("Next file was skipped: " + file.getName());
+                            slackNotification.sendMessageToNotificationsChannel("Next file was skipped: " + file.getName());
+                        }
                     }
                 }
+            } catch (Exception e) {
+                slackNotification.sendMessageToNotificationsChannel("Parsing task terminated: " + e.getMessage());
+                logger.info(e.getMessage());
             }
         }
     }
@@ -75,10 +82,10 @@ public class DataParserTask {
         StringBuilder homePath = new StringBuilder();
         String osName = System.getProperty("os.name");
         if (osName.toLowerCase().startsWith("windows")) {
-            homePath.append(windowsPath)
+            homePath.append(env.getProperty("ftp.action.data.windows.home.path"))
                     .append(File.separator);
         } else if (osName.toLowerCase().startsWith("linux")) {
-            homePath.append(linuxPath)
+            homePath.append(env.getProperty("ftp.action.data.linux.home.path"))
                     .append(File.separator);
         }
         return homePath;
