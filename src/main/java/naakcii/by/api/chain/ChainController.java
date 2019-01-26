9d2cde52d795976f13378de6621b25cb196ec753
KaterinaLupacheva <39033545+KaterinaLupacheva@users.noreply.github.com>
2019-01-26 17:06:01 +0300

    Swagger config

--- src/main/java/naakcii/by/api/chain/ChainController.java
+++ src/main/java/naakcii/by/api/chain/ChainController.java
@@ -1,5 +1,7 @@
 package naakcii.by.api.chain;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import naakcii.by.api.config.ApiConfigConstants;
 
 import java.util.List;
 
+@Api(description = "REST API для сущности Chain")
 @RestController
 @RequestMapping({"/chains"})
 public class ChainController {
@@ -21,6 +24,7 @@ public class ChainController {
     }
 
     @GetMapping(produces = ApiConfigConstants.API_V_2_0)
+    @ApiOperation("Возвращает список всех торговых сетей")
     public List<ChainDTO> getAllChains() {
         return chainService.getAllChains();
     }
