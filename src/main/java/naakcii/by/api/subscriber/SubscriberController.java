a6e93b5c3fa09e7692683733409ee11c7f0478cf
KaterinaLupacheva <39033545+KaterinaLupacheva@users.noreply.github.com>
2019-01-26 17:09:05 +0300

    Swagger config

--- src/main/java/naakcii/by/api/subscriber/SubscriberController.java
+++ src/main/java/naakcii/by/api/subscriber/SubscriberController.java
@@ -1,5 +1,7 @@
 package naakcii.by.api.subscriber;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import naakcii.by.api.config.ApiConfigConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "REST API для сущности Subscriber")
 @RestController
 @RequestMapping({"/subscribers"})
 public class SubscriberController {
@@ -15,6 +18,7 @@ public class SubscriberController {
     private SubscriberService subscriberService;
 
     @PostMapping(produces = ApiConfigConstants.API_V_2_0)
+    @ApiOperation("Создает нового подписчика сервиса")
     public SubscriberDTO subscribe(@RequestBody SubscriberDTO subscriberDTO) {
         return subscriberService.save(subscriberDTO.getEmail());
     }
