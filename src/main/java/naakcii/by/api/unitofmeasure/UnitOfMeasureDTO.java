c3497005f6829e801ad2f16ecf9360534357deca
KaterinaLupacheva <39033545+KaterinaLupacheva@users.noreply.github.com>
2019-01-26 17:09:45 +0300

    Swagger config

--- src/main/java/naakcii/by/api/unitofmeasure/UnitOfMeasureDTO.java
+++ src/main/java/naakcii/by/api/unitofmeasure/UnitOfMeasureDTO.java
@@ -2,6 +2,7 @@ package naakcii.by.api.unitofmeasure;
 
 import java.math.BigDecimal;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,8 +11,10 @@ import lombok.Setter;
 @Setter
 @Getter
 public class UnitOfMeasureDTO {
-	
+
+	@ApiModelProperty(notes = "Наименование единицы измерения", example="Кг")
 	private String name;
+	@ApiModelProperty(notes = "Шаг изменения единицы измерения", example="0.1")
 	private BigDecimal step;
 	
 	public UnitOfMeasureDTO(UnitOfMeasure unitOfMeasure) {
