3a84886a655ed1ebefec925b838989c48411d50b
KaterinaLupacheva <39033545+KaterinaLupacheva@users.noreply.github.com>
2019-01-26 17:07:52 +0300

    Swagger config

--- src/main/java/naakcii/by/api/statistics/StatisticsController.java
+++ src/main/java/naakcii/by/api/statistics/StatisticsController.java
@@ -1,11 +1,14 @@
 package naakcii.by.api.statistics;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import naakcii.by.api.config.ApiConfigConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "REST API для сущности Statistics")
 @RestController
 @RequestMapping({"/statistics"})
 public class StatisticsController {
@@ -14,6 +17,7 @@ public class StatisticsController {
     private StatisticsService statisticsService;
 
     @GetMapping(produces = ApiConfigConstants.API_V_2_0)
+    @ApiOperation("Возвращает статистику по сервису НаАкции.Бел")
     public StatisticsDTO getStatistics() {
         return statisticsService.findCurrentStatistics();
     }
--- src/main/java/naakcii/by/api/statistics/StatisticsDTO.java
+++ src/main/java/naakcii/by/api/statistics/StatisticsDTO.java
@@ -1,5 +1,6 @@
 package naakcii.by.api.statistics;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,10 +10,15 @@ import lombok.Setter;
 @Getter
 public class StatisticsDTO {
 
+    @ApiModelProperty(notes = "Id статистики", example="1L")
     private Long id;
+    @ApiModelProperty(notes = "Количество торговых сетей", example="10")
     private Integer chainQuantity;
+    @ApiModelProperty(notes = "Количество товаров со скидкой", example="15")
     private Integer discountedProducts;
+    @ApiModelProperty(notes = "Средний процент скидки", example="10")
     private Integer averageDiscountPercentage;
+    @ApiModelProperty(notes = "Дата создания записи статистики", example="1508484583267")
     private Long creationDateMillis;
 
     public StatisticsDTO(Statistics statistics) {
