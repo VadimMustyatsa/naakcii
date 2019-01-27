819f7d96541f36e0a8ed5d79590cd872a806408f
AlexandrKuchura <kuchura0409@gmail.com>
2019-01-27 17:12:10 +0300

    Subcategory tests were updated

--- src/test/java/naakcii/by/api/subcategory/SubcategoryControllerTest.java
+++ src/test/java/naakcii/by/api/subcategory/SubcategoryControllerTest.java
@@ -1,35 +1,34 @@
 package naakcii.by.api.subcategory;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Collections;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @RunWith(MockitoJUnitRunner.class)
 public class SubcategoryControllerTest {
 
-	private SubcategoryController subcategoryController;
-	private List<SubcategoryDTO> subcategoryDTOs;
-	
-	@Mock
-	private SubcategoryService subcategoryService;
-	
-	@Before
-	public void setUp() {
-		subcategoryController = new SubcategoryController(subcategoryService);
-	}
-	
-	@Test
-	public void test_get_all_subcategories_by_category_id() {
-		Long categoryId = 500L;
-		when(subcategoryService.getAllSubcategoriesByCategoryId(categoryId)).thenReturn(subcategoryDTOs);
-		subcategoryController.getAllSubcategoriesByCategoryId(categoryId);
-		verify(subcategoryService).getAllSubcategoriesByCategoryId(categoryId);
-	}
+    private SubcategoryController subcategoryController;
+
+    @Mock
+    private SubcategoryService subcategoryService;
+
+    @Before
+    public void setUp() {
+        subcategoryController = new SubcategoryController(subcategoryService);
+    }
+
+    @Test
+    public void test_get_all_subcategories_by_category_id() {
+        Long categoryId = 500L;
+        when(subcategoryService.getAllSubcategoriesByCategoryId(categoryId)).thenReturn(Collections.emptyList());
+        subcategoryController.getAllSubcategoriesByCategoryId(categoryId);
+        verify(subcategoryService).getAllSubcategoriesByCategoryId(categoryId);
+    }
 }
