b7217b1c71bd462bfd8085880ebd48684f3f37f3
AlexandrKuchura <kuchura0409@gmail.com>
2019-01-27 20:17:19 +0300

    Product tests were updated

--- src/test/java/naakcii/by/api/product/ProductRepositoryTest.java
+++ src/test/java/naakcii/by/api/product/ProductRepositoryTest.java
@@ -1,10 +1,11 @@
 package naakcii.by.api.product;
 
-import static org.junit.Assert.assertTrue;
-
-import java.math.BigDecimal;
-import java.util.Calendar;
-
+import naakcii.by.api.category.Category;
+import naakcii.by.api.country.Country;
+import naakcii.by.api.country.CountryCode;
+import naakcii.by.api.subcategory.Subcategory;
+import naakcii.by.api.unitofmeasure.UnitCode;
+import naakcii.by.api.unitofmeasure.UnitOfMeasure;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,76 +18,66 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import naakcii.by.api.category.Category;
-import naakcii.by.api.chain.Chain;
-import naakcii.by.api.chainproduct.ChainProduct;
-import naakcii.by.api.chainproducttype.ChainProductType;
-import naakcii.by.api.country.Country;
-import naakcii.by.api.country.CountryCode;
-import naakcii.by.api.subcategory.Subcategory;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:application-unit-test.properties")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class ProductRepositoryTest {
-	/*
-	@Autowired
-	private TestEntityManager testEntityManager;
-	
-	@Autowired
-	private ProductRepository productRepository;
-		
-	private Product product;
-	
-	@Before
-	public void setUp() {
-		Category category = new Category("Category name", true);
-		category.setIcon("Category icon");
-		category.setPriority(5);
-		Subcategory subcategory = new Subcategory("Subcategory name", category, true);
-		subcategory.setPriority(10);
-		Chain chain = new Chain("Chain name", "Chain link", true);
-		chain.setLogo("Chain logo");
-		ChainProductType actionType = new ChainProductType("Action type name");
-		actionType.setTooltip("Action type tooltip text.");
-		Country country = new Country(CountryCode.BG);
-		product = new Product("1000123456789", "Product name", Unit.KG, true, subcategory);
-		product.setUnit(Unit.KG);
-		product.setPicture("Product picture");
-		product.setManufacturer("Manufacturer");
-		product.setBrand("Brand");
-		product.setCountryOfOrigin(country);
-		testEntityManager.persist(chain);
-		testEntityManager.persist(category);
-		testEntityManager.persist(actionType);
-		testEntityManager.persist(country);
-		Calendar startDate = Calendar.getInstance();
-		Calendar endDate = Calendar.getInstance();
-		endDate.add(Calendar.MONTH, 1);
-		ChainProduct action = new ChainProduct(product, chain, new BigDecimal("7.50"), actionType, startDate, endDate);
-		action.setDiscountPercent(new BigDecimal("25"));
-		action.setBasePrice(new BigDecimal("10.00"));
-		testEntityManager.flush();
-		testEntityManager.detach(actionType);
-		testEntityManager.detach(category);
-		testEntityManager.detach(chain);
-	}
-	
-	@Test
-	public void test_soft_delete() {
-		int numberOfUpdatedRows = productRepository.softDelete(product.getId());
-		assertTrue("Number of updated rows in the database should be equal to 1, as 1 entity has been modified.", numberOfUpdatedRows == 1);
-	}
-	
-	@Test
-	public void test_soft_delete_with_wrong_id() {
-		int numberOfUpdatedRows = productRepository.softDelete(product.getId() + 10);
-		assertTrue("Number of updated rows in the database should be equal to 0, as nothing has been modified.", numberOfUpdatedRows == 0);
-	}
-	
-	@After
-	public void tearDown() {
-		product = null;
-	}*/
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private Product product;
+
+    @Before
+    public void setUp() {
+        Category category = new Category("Рыба и морепродукты", true);
+        testEntityManager.persist(category);
+        Subcategory subcategory = new Subcategory("Ягоды, фрукты", true, category);
+        testEntityManager.persist(subcategory);
+        Country country = new Country(CountryCode.BG);
+        testEntityManager.persist(country);
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure(UnitCode.KG);
+        testEntityManager.persist(unitOfMeasure);
+        product = new Product("1000123456789", "Минтай с/м б/г Вес", unitOfMeasure, true, subcategory);
+        product.setCountryOfOrigin(country);
+        testEntityManager.persist(product);
+        Product secondProduct = new Product("1003732742789", "Хек с/м б/г Вес", unitOfMeasure, true, subcategory);
+        testEntityManager.persist(secondProduct);
+        testEntityManager.flush();
+    }
+
+    @Test
+    public void test_soft_delete() {
+        int numberOfUpdatedRows = productRepository.softDelete(product.getId());
+        assertTrue("Number of updated rows in the database should be equal to 1, as 1 entity has been modified.", numberOfUpdatedRows == 1);
+    }
+
+    @Test
+    public void test_soft_delete_with_wrong_id() {
+        int numberOfUpdatedRows = productRepository.softDelete(product.getId() + 10);
+        assertTrue("Number of updated rows in the database should be equal to 0, as nothing has been modified.", numberOfUpdatedRows == 0);
+    }
+
+    @Test
+    public void test_find_by_name_and_barcode_and_unit_of_measure() {
+        Optional<Product> optionalProduct = productRepository
+                .findByNameAndBarcodeAndUnitOfMeasure(product.getName(), product.getBarcode(), product.getUnitOfMeasure());
+        assertTrue("Product was found in the database", optionalProduct.isPresent());
+        assertEquals("Болгария", optionalProduct.get().getCountryOfOrigin().getName());
+
+    }
+
+    @After
+    public void tearDown() {
+        product = null;
+    }
 }
