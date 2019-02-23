accc183c352c2d65ce460a0cda3132a64d6ea6ec
GutsulyakSergey <kontrolnaia-su@tut.by>
2019-02-23 16:20:18 +0300

    товар в корзине под новую модель

--- front/src/app/shopping-list-page/shopping-list/cart-line/cart-line.component.ts
+++ front/src/app/shopping-list-page/shopping-list/cart-line/cart-line.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import {BreakPointCheckService} from '../../../shared/services/breakpoint-check.service';
 import {Cart, CartLine} from '../../../shared/cart/cart.model';
 import setImgStyles from '../../../shared/utils/setImgStyles';
+import {ChainProduct} from '../../../shared/model/chain-product.model';
 @Component({
   selector: 'app-cart-line',
   templateUrl: './cart-line.component.html',
@@ -11,30 +12,33 @@ import setImgStyles from '../../../shared/utils/setImgStyles';
 export class CartLineComponent implements OnInit {
   @Input() curCartline: CartLine;
   @Output() delItem: EventEmitter<void> = new EventEmitter();
-
+  product: ChainProduct;
   constructor(public breakPointCheckService: BreakPointCheckService,
               public cart: Cart
     ) {
   }
 
   ngOnInit() {
+    this.product = this.curCartline.product;
   }
   public subItem(): void {
-    if (this.curCartline.quantity > 1) {
-      this.cart.updateQuantity(this.curCartline.product, Number(this.curCartline.quantity - 1));
+    if (this.curCartline.quantity > this.product.changeStep) {
+      this.cart.updateQuantity(this.product, Number(Math.round((this.curCartline.quantity - this.product.changeStep) * 10) / 10));
     }
   }
 
   public addItem(): void {
-    this.cart.updateQuantity(this.curCartline.product, Number(this.curCartline.quantity + 1));
+    this.cart.updateQuantity(this.product, Number(Math.round((this.curCartline.quantity + this.product.changeStep) * 10) / 10));
   }
 
   public deleteCartLine(): void {
-    this.cart.removeLine(this.curCartline.product.productId);
+    this.cart.removeLine(this.product.productId);
     this.delItem.emit();
   }
 
   public setImgStyles(pict: string): {} {
     return setImgStyles(pict);
   }
+
+  // public getSumBasePrice(): number{}
 }
