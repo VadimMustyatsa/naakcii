913531f79592affaef3c87937428d5ab388e2322
griboedovvladimir <likecoffee@yandex.ru>
2019-03-06 23:30:32 +0300

    Native brouser's tootips

--- front/src/app/shared/directives/tooltip/tooltip.directive.ts
+++ front/src/app/shared/directives/tooltip/tooltip.directive.ts
@@ -4,18 +4,15 @@ import {Directive, HostListener, Input, ElementRef} from '@angular/core';
   selector: '[tooltip]'
 })
 export class TooltipDirective {
-  showingTooltip:any;
   constructor(private elementRef:ElementRef) {
   }
   @Input('tooltip')
   message:string;
 
   @HostListener('mouseenter') mouseIn() {
-    this.elementRef.nativeElement.classList.add("tooltip");
-    this.elementRef.nativeElement.style.setProperty('--halfElemWidth', this.elementRef.nativeElement.getBoundingClientRect().width/2 + 'px');
-    this.elementRef.nativeElement.style.setProperty('--elemWidth', - this.elementRef.nativeElement.getBoundingClientRect().width + 'px');
+    this.elementRef.nativeElement.setAttribute('title', this.message)
   }
   @HostListener('mouseleave') mouseOut() {
-    this.elementRef.nativeElement.classList.remove("tooltip");
+    this.elementRef.nativeElement.setAttribute('title', '')
   }
 }
