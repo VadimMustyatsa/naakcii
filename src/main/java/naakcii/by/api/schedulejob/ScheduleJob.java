76fa6c79e90d7398f1cdd92e8a5db866dccd26de
AliaksandrKuchura <kuchura0409@gmail.com>
2019-04-09 22:06:46 +0300

    Jobs models were added

---
+++ src/main/java/naakcii/by/api/schedulejob/ScheduleJob.java
@@ -0,0 +1,49 @@
+package naakcii.by.api.schedulejob;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import naakcii.by.api.schedulejobtype.ScheduleJobType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(exclude = {"id"})
+@Entity
+@Table(name = "SCHEDULE_JOB")
+public class ScheduleJob implements Serializable {
+
+    private static final long serialVersionUID = -4053114041932518831L;
+
+    @Id
+    @NotNull
+    @Column(name = "SCHEDULE_JOB_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "SCHEDULE_JOB_NAME", unique = true, updatable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "SCHEDULE_JOB_CRON_EXPRESSION")
+    private String cronExpression;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "SCHEDULE_JOB_TYPE_ID")
+    ScheduleJobType scheduleJobType;
+
+}
---
+++ src/main/java/naakcii/by/api/schedulejobtype/ScheduleJobType.java
@@ -0,0 +1,43 @@
+package naakcii.by.api.schedulejobtype;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(exclude = {"id"})
+@Entity
+@Table(name = "SCHEDULE_JOB_TYPE")
+public class ScheduleJobType implements Serializable {
+
+    private static final long serialVersionUID = -397384728748404791L;
+
+    @Id
+    @NotNull
+    @Column(name = "SCHEDULE_JOB_TYPE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Byte id;
+
+    @Column(name = "SCHEDULE_JOB_TYPE_NAME")
+    private String name;
+
+    @Column(name = "SCHEDULE_JOB_TYPE_CLASS")
+    private String className;
+
+    public ScheduleJobType(String name, String className) {
+        this.name = name;
+        this.className = className;
+    }
+}
