c42bf45b3257e2874d30c172524b11117cf94335
AliaksandrKuchura <kuchura0409@gmail.com>
2019-04-09 22:14:34 +0300

    Quartz config and properties were created. BeanJobFactory for quartz was added. pom was updated

---
+++ src/main/java/naakcii/by/api/util/scheduler/AutoWiringSpringBeanJobFactory.java
@@ -0,0 +1,23 @@
+package naakcii.by.api.util.scheduler;
+
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+public final class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements
+        ApplicationContextAware {
+
+    private ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Override
+    protected Object createJobInstance(final TriggerFiredBundle bundle) {
+        return context.getBean(bundle.getJobDetail().getJobClass());
+    }
+}
